b363d34ea0bc1789ac97bdda2f20f16e69bebe93
Sean McIntyre <boxysean@gmail.com>
2013-02-03 10:27:42 -0500

    fixed apache issues

--- wsgi_handler.py
+++ wsgi_handler.py
@@ -2,6 +2,7 @@ import os, sys
 
 #sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 import django.core.handlers.wsgi
 
